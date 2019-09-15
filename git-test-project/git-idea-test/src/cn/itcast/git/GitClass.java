24475e5787c26557ede0768830bd4045ee25de03
itcast <itcast@hh.com>
2019-09-15 16:57:25 +0800

    first commit

---
+++ git-test-project/git-idea-test/src/cn/itcast/git/GitClass.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class GitClass {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
